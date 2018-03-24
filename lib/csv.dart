160e274a3be423926c28a0d7b78d93edfc1f4fcd
Aaron Madison <aaron.l.madison@gmail.com>
2018-03-24 14:04:01 -0500

    Feature: added Map to CSV converter

--- lib/csv.dart
+++ lib/csv.dart
@@ -8,6 +8,7 @@ import 'csv_settings_autodetection.dart';
 
 part 'csv_to_list_converter.dart';
 part 'list_to_csv_converter.dart';
+part 'map_to_csv_converter.dart';
 
 /// The RFC conform default value for field delimiter.
 const String defaultFieldDelimiter = ',';
